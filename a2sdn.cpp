893c49a248ac79e97a20ab5f8033a7875e396627
Toasterstein <nklapstein@gmail.com>
2018-10-11 20:37:14 -0400

    adding `main`

--- a2sdn.cpp
+++ a2sdn.cpp
@@ -4,3 +4,15 @@
  * @author Nathan Klapstein (nklapste)
  * @version 0.0.0
  */
+
+
+/**
+ * Main entry point for a2sdn.
+ *
+ * @param argc
+ * @param argv
+ * @return
+ */
+int main(int argc, char **argv) {
+    return 0;
+}
