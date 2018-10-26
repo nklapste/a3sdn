47b4ef71ccc2a3e563006e4b57e65064015a408f
nklapste <nklapste@ualberta.ca>
2018-10-26 17:49:41 -0600

    refactoring

--- switch.h
+++ switch.h
@@ -45,10 +45,13 @@ typedef std::vector<flowEntry> FlowTable;
 class Switch {
 public:
     Switch(string &switchId, string &leftSwitchId, string &rightSwitchId, string &trafficFile, uint ipLow, uint ipHigh);
+
     Switch(uint switchId, uint neighbors, uint ipLow, uint ipHigh);
 
     void list();
+
     void start();
+
 private:
     /**
      * ID of the switch itself
