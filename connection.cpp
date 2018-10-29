9dd77c758dc904bacf15a6a15f35ce40c599c034
nklapste <nklapste@ualberta.ca>
2018-10-29 06:59:13 -0600

    refactoring

--- connection.cpp
+++ connection.cpp
@@ -15,79 +15,7 @@
 using namespace std;
 
 /**
- * Make a FIFO for a {@code Connection}.
- *
- * @param FIFOName the name to be given to the FIFO.
- */
-void Connection::makeFIFO(string &FIFOName) {
-    printf("DEBUG: making FIFO at: %s\n", FIFOName.c_str());
-    int status = mkfifo(FIFOName.c_str(), S_IRUSR | S_IWUSR | S_IRGRP |
-                                          S_IWGRP | S_IROTH | S_IWOTH);
-    if (errno || status == -1) {
-        perror("ERROR: error creating FIFO connection");
-        errno = 0;
-    }
-}
-
-/**
- * Opens a Connection for reading a switch with id.
- *
- * @return
- */
-int Connection::openReceiveFIFO() {
-    printf("DEBUG: opening receiveFIFO: %s\n", receiveFIFOName.c_str());
-    int i = open(receiveFIFOName.c_str(), O_RDONLY | O_NONBLOCK);
-    if (!i) {
-        perror("ERROR: opening receive FIFO");
-    }
-    return i;
-}
-
-/**
- * Opens a Connection for writing a switch with id.
- *
- * @return
- */
-int Connection::openSendFIFO() {
-    printf("DEBUG: opening sendFIFO: %s\n", receiveFIFOName.c_str());
-    int i = open(sendFIFOName.c_str(), O_WRONLY | O_NONBLOCK);
-    if (!i) {
-        perror("ERROR: opening send FIFO");
-    }
-    return i;
-}
-
-
-/**
- * Getter for the Receiving FIFO filename/pipename.
- *
- * @return {@code std::string}
- */
-string Connection::getReceiveFIFOName() {
-    return receiveFIFOName;
-}
-
-/**
- * Getter for the Sending FIFO filename/pipename.
- *
- * @return {@code std::string}
- */
-string Connection::getSendFIFOName() {
-    return sendFIFOName;
-}
-
-/**
- * Generate a FIFO name based from the sender's id and receiver's id.
- *
- * @param senderId {@code uint}
- * @param receiverId {@code uint}
- * @return
- */
-const string Connection::makeFIFOName(uint senderId, uint receiverId) {
-    return "fifo-" + std::to_string(senderId) + "-" + std::to_string(receiverId);
-}
-
-/**
+ * Initialize a Connection between either a Controller and a Switch or a Switch and another Switch.
  *
  * @param srcID {@code uint}
  * @param dstID {@code uint}
@@ -104,4 +32,77 @@ Connection::Connection(uint srcID, uint dstID) {
     printf("INFO: Making Connection:\n"
            "\tsrc: %u dst: %u receiveFIFO: %s\n", srcID, dstID, receiveFIFOName.c_str());
     makeFIFO(receiveFIFOName);
+}
+
+/**
+ * Getter for the ReceiveFIFO filename.
+ *
+ * @return {@code std::string}
+ */
+string Connection::getReceiveFIFOName() {
+    return receiveFIFOName;
+}
+
+/**
+ * Getter for the SendFIFO filename.
+ *
+ * @return {@code std::string}
+ */
+string Connection::getSendFIFOName() {
+    return sendFIFOName;
+}
+
+/**
+ * Opens a Connection for reading a switch with id.
+ *
+ * @return {@code int} the file description of the ReceiveFIFO.
+ */
+int Connection::openReceiveFIFO() {
+    printf("DEBUG: opening receiveFIFO: %s\n", receiveFIFOName.c_str());
+    int i = open(receiveFIFOName.c_str(), O_RDONLY | O_NONBLOCK);
+    if (!i) {
+        perror("ERROR: opening receive FIFO");
+    }
+    return i;
+}
+
+/**
+ * Opens a Connection for writing a switch with id.
+ *
+ * @return {@code int} the file description of the SendFIFO.
+ */
+int Connection::openSendFIFO() {
+    printf("DEBUG: opening sendFIFO: %s\n", receiveFIFOName.c_str());
+    int i = open(sendFIFOName.c_str(), O_WRONLY | O_NONBLOCK);
+    if (!i) {
+        perror("ERROR: opening send FIFO");
+    }
+    return i;
+}
+
+/**
+ * Generate a FIFO name based from the sender's id and receiver's id.
+ *
+ * @param senderId {@code uint}
+ * @param receiverId {@code uint}
+ * @return
+ */
+const string Connection::makeFIFOName(uint senderId, uint receiverId) {
+    return "fifo-" + std::to_string(senderId) + "-" + std::to_string(receiverId);
+}
+
+/**
+ * Make a FIFO for a {@code Connection}.
+ *
+ * @param FIFOName the name to be given to the FIFO.
+ */
+void Connection::makeFIFO(string &FIFOName) {
+    printf("DEBUG: making FIFO at: %s\n", FIFOName.c_str());
+    int status = mkfifo(FIFOName.c_str(), S_IRUSR | S_IWUSR | S_IRGRP |
+                                          S_IWGRP | S_IROTH | S_IWOTH);
+    // TODO: ignore if FIFO is already made
+    if (errno || status == -1) {
+        perror("ERROR: error creating FIFO connection");
+        errno = 0;
+    }
 }
