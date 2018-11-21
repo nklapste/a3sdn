241b90245acacbfac8ec6d13f2d4ff6b000cbab6
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-21 15:05:29 -0700

    improving logging

--- connection.cpp
+++ connection.cpp
@@ -74,8 +74,13 @@ int Connection::openReceiveFIFO() {
     printf("DEBUG: opening receiveFIFO: %s\n", getReceiveFIFOName().c_str());
     int fd = open(getReceiveFIFOName().c_str(), O_RDONLY | O_NONBLOCK);
     if (errno || fd == -1) {
-        perror("ERROR: opening receiveFIFO");
-        exit(errno);
+        perror("ERROR: opening receiveFIFO\n");
+        if (errno != ENXIO) {
+            printf("ERROR: unexpected error opening receiveFIFO\n");
+            exit(errno);
+        } else {
+            printf("WARNING: failed to open receiveFIFO\n");
+        }
     }
     return fd;
 }
@@ -90,8 +95,13 @@ int Connection::openSendFIFO() {
     printf("DEBUG: opening sendFIFO: %s\n", getSendFIFOName().c_str());
     int fd = open(getSendFIFOName().c_str(), O_WRONLY | O_NONBLOCK);
     if (errno || fd == -1) {
-        perror("ERROR: opening sendFIFO");
-        exit(errno);
+        perror("ERROR: opening sendFIFO\n");
+        if (errno != ENXIO) {
+            printf("ERROR: unexpected error opening sendFIFO\n");
+            exit(errno);
+        } else {
+            printf("WARNING: failed to open sendFIFO\n");
+        }
     }
     return fd;
 }
